68b0237f9a1647addb346337e5e6af46cea10d09
stormwarestudios <peter.kingsbury@gmail.com>
2016-03-28 12:44:21 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var uuid = require('uuid');
+
+module.exports = {
+  generate: function() {
+    var unordered = uuid.v4();
+    return unordered.substr(14, 4) + unordered.substr(9, 4) + unordered.substr(0, 8) + unordered.substr(19, 4) + unordered.substr(24, unordered.length);
+  },
+
+  toBinary16: function(orderedUuid) {
+    return new Buffer(orderedUuid, 'hex');
+  },
+
+  fromBinary16: function(binaryOrderedUuid) {
+    return binaryOrderedUuid.toString('hex');
+  }
+};
